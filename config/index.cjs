5ae205069bf5754822835270c6274a4bc5cf35ba
djksvhgiur <116484618+djksvhgiur@users.noreply.github.com>
2022-10-23 20:34:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '欣欣子',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oaix_55NINZ0_7eXymJ57YRl6Mi8',
+      id: 'oaix_5yr1GDmfXMZViv9NlTAYUpI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'V077WdDL0su3E_pF4bI8MCPLNTNJLnwmVFfyfXuFYaQ',
      
