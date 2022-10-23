cd3ca4f2a046b2be18eff605539d73260c93a637
djksvhgiur <116484618+djksvhgiur@users.noreply.github.com>
2022-10-23 17:45:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '欣欣子',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oaix_55NINZ0_7eXymJ57YRl6Mi8',
+      id: 'oaix_5yr1GDmfXMZViv9NlTAYUpI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'kQVRtp2M1MqU5-tLjKr9iRagJbF-4HHfobrJu0-1EBE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -40,7 +40,7 @@ const USER_CONFIG = {
         },
       ],
       // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [我们在一起已经111天}
+      customizedDateList: ['我们在一起已经111天'}
         // 在一起的日子
         { keyword: 'love_day', date: '2022-07-04' },
         // 结婚纪念日
