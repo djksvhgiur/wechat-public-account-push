d086e1029166af1fe7d63da575a6260ddd56b20d
djksvhgiur <116484618+djksvhgiur@users.noreply.github.com>
2022-10-25 11:11:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oaix_55NINZ0_7eXymJ57YRl6Mi8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'V077WdDL0su3E_pF4bI8MCPLNTNJLnwmVFfyfXuFYaQ',
+      useTemplateId: '0001',
      
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: ['2022-07-04']
